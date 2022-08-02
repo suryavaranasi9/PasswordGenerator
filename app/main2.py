0e2ab0e07d1ecb55f5def10c82c7eb698205651d
Surya Varanasi <30657946+suryavaranasi9@users.noreply.github.com>
2022-08-02 16:52:38 +0530

    Update main2.py

--- app/main2.py
+++ app/main2.py
@@ -99,6 +99,12 @@ async def root():
                         ReDoc
                     </a>
                 </div>
+                <div class="mt-6">
+                    <a href="http://127.0.0.1:8000/password"
+                        class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black text-opacity-75 bg-white bg-opacity-75 sm:bg-opacity-25 sm:hover:bg-opacity-50">
+                        Output API Response Page
+                    </a>
+                </div>
             </div>
         </main>
     </body>
